05b4493fa56dec1c60b1f06513b4dd7be0623b92
Magda <kimagdalenasztuk@gmail.com>
2025-11-29 14:41:10 +0100

    front-end

---
+++ app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import os
+import datetime
+
+st.title("Vet Assistant")
+
+# Folder na nagrania
+SAVE_DIR = "Recordings"
+os.makedirs(SAVE_DIR, exist_ok=True)
+
+# Nagrywanie
+audio_file = st.audio_input("Nagraj wizyte")
+
+if audio_file:
+    st.audio(audio_file)
+
+    # Generowanie unikalnej nazwy pliku
+    timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+    filename = f"recording_{timestamp}.wav"
+    filepath = os.path.join(SAVE_DIR, filename)
+
+    # Zapis do pliku
+    with open(filepath, "wb") as f:
+        f.write(audio_file.getvalue())
+
+    st.success(f"Zapisano nagranie jako: {filename}")
+    st.write("Ścieżka:", filepath)
